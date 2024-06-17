71bea0223b74b275de13bac3403acd2e0dc38007
TaraMcGruder <tara.mcgruder@mail.utoronto.ca>
2024-06-16 22:10:10 -0400

    Mapping out data with a few hiccups

--- Occurrence_analyses.R
+++ Occurrence_analyses.R
@@ -1,5 +1,9 @@
 # Library ----
 library(dplyr)
+library(ggplot2)
+library(sf)
+library(rnaturalearth)
+library(rnaturalearthdata)
 
 # Read in CSV file (GBIF) ----
 occurrence_data <- read.csv("C:/Users/taram/Documents/Lab Analysis/NSERC USRA/Clarkia_OccurrenceData_GBIF/GBIF_occurrence_cleaned.csv")
@@ -60,6 +64,8 @@ print(CCH2_species_count_df)
 
 
 
+
+
 ## Read in CSV file (PNW Herbaria) ----
 PNW_occurrence_data <- read.csv("C:/Users/taram/Documents/Lab Analysis/NSERC USRA/Clarkia_Occurrence_Data_PNW/PNW_occurrence_cleaned.csv")
 
@@ -79,5 +85,101 @@ print(PNW_species_count_df)
 
 
 
-# Map ----
+# Maps ----
+## GBIF Map ----
+#convert to spatial format but first get rid of NA values 
+  #but does this mean I am losing data???
+#Getting rid of NA values 
+occurrence_speciesclean <- occurrence_speciesclean %>%
+  filter(!is.na(decimalLatitude) & !is.na(decimalLongitude))
+#Converting to spatial format 
+occurrence_sf <- st_as_sf(occurrence_speciesclean, coords = c("decimalLongitude", "decimalLatitude"), crs = 4326)
+#Plotting
+world <- ne_countries(scale = "medium", returnclass = "sf")
+
+ggplot(data = world) +
+  geom_sf() +
+  geom_sf(data = occurrence_sf, aes(color = species), size = 2, alpha = 0.7) +
+  theme_minimal() +
+  labs(title = "GBIF Occurrence Map: Genus Clarkia",
+       x = "Longitude",
+       y = "Latitude",
+       color = "Species")
+
+## INAT Map ----
+#Removing lat/long rows with missing data 
+INAT_occurrence_data_columnclean <- INAT_occurrence_data_columnclean %>%
+  filter(!is.na(latitude) & !is.na(longitude))
+#Convert to spatial format 
+INAT_occurrence_sf <- st_as_sf(INAT_occurrence_data_columnclean, coords = c("longitude", "latitude"), crs = 4326)
+
+#Plotting
+world <- ne_countries(scale = "medium", returnclass = "sf")
+
+ggplot(data = world) +
+  geom_sf() +
+  geom_sf(data = INAT_occurrence_sf, aes(color = scientific_name), size = 2, alpha = 0.7) +
+  theme_minimal() +
+  labs(
+    title = "INAT Occurrence Map: Genus Clarkia",
+    x = "Longitude",
+    y = "Latitude",
+    color = "Species"
+  )
+
+
+## CCH2 Map ---- 
+#Removing lat/long rows with missing data
+CCH2_occurrence_data_columnclean <- CCH2_occurrence_data_columnclean %>%
+  filter(!is.na(decimalLatitude) & !is.na(decimalLongitude))
+#Convert to spatial format
+CCH2_occurrence_sf <- st_as_sf(CCH2_occurrence_data_columnclean, coords = c("decimalLongitude", "decimalLatitude"), crs = 4326)
+
+#Plotting
+world <- ne_countries(scale = "medium", returnclass = "sf")
+
+ggplot(data = world) +
+  geom_sf() +
+  geom_sf(data = CCH2_occurrence_sf, aes(color = scientificName), size = 2, alpha = 0.7) +
+  theme_minimal() +
+  labs(
+    title = "CCH2 Occurrence Map: Genus Clarkia",
+    x = "Longitude",
+    y = "Latitude",
+    color = "Species"
+  ) +
+  theme(
+    legend.position = "none",
+    legend.title = element_text(size = 10),
+    legend.text = element_text(size = 8),
+    legend.key.size = unit(0, "cm"),
+  )
+#It literally looks so ugly and smushed. Idk, fix this Tara 
+
+## PNW Map ----
+# Remove rows with missing values 
+PNW_occurrence_data_columnclean <- PNW_occurrence_data %>%
+  filter(!is.na(Decimal.Latitude) & !is.na(Decimal.Longitude))
+#Convert to spatial format 
+PNW_occurrence_sf <- st_as_sf(PNW_occurrence_data_columnclean, coords = c("Decimal.Longitude", "Decimal.Latitude"), crs = 4326)
+
+#Plotting 
+world <- ne_countries(scale = "medium", returnclass = "sf")
+
+ggplot(data = world) +
+  geom_sf() +
+  geom_sf(data = PNW_occurrence_sf, aes(color = Scientific.Name), size = 2, alpha = 0.7) +
+  theme_minimal() +
+  labs(
+    title = "PNW Occurrence Map: Genus Clarkia",
+    x = "Longitude",
+    y = "Latitude",
+    color = "Species"
+  )
+#Idk why I can't see the map 
+
+
+
+
+
 
